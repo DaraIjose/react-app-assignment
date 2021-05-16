766fd11e1849f9d8095439c7bafd538034ffc6ec
Dara-Ijose <ijosedara213@yahoo.com>
2021-05-16 11:47:08 -0500

    responsive react app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import background from "./Images/bg-intro-desktop.png";
-
 import './App.css';
 
 function App() {
   return (
 
-        <main>
-
-          <img src={background} alt="background design"/>
-
-          <div className="container">
-
-            <div className="sec-1">
-              <button className="button1">Try it free 7 days then $20/mo. thereafter</button>
-            </div>
+        <main className="container">
             <div className="sec-2">
               <h1 className="learn">Learn to code by watching others</h1>
               <p className="see">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
             </div>
+
             <div className="sec-3">
-              <input type="" name="firstname" placeholder="First Name"/>
-              <input type="text" name="lastname" placeholder="Last Name"/>
-              <input type="text" name="email" placeholder="Email Address"/>
-              <input type="text" name="password" placeholder="Password"/>
-              <div>
-                <button className="button2">CLAIM YOUR FREE TRIAL </button>
+              <div className="">
+               
+                <div className="button1">
+                  <a href="/"><strong>Try it free 7 days</strong> then $20/mo. thereafteer</a>
+                </div>
               </div>
-              <div>
-                <p className="footer">By clicking the button you are agreeing to the Terms and Services</p>
-              </div>
+              <form action="" className="form-container">
+                <input type="text" name="firstname" placeholder="First Name"/>
+                <input type="text" name="lastname" placeholder="Last Name"/>
+                <input type="email" name="email" placeholder="Email Address"/>
+                <input type="password" name="password" placeholder="Password"/>
+                <button type="submit" className="button2">CLAIM YOUR FREE TRIAL </button>
+              </form>
+                <p className="terms">
+                  By clicking the button you are agreeing to the 
+                  <a href="/">Terms and Services</a>
+                </p>
             </div>
-
-          </div>
-
         </main>
 
   );
